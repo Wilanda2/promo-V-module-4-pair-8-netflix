262bf4397cc226d8b7ff72d92dccac4758ea40fd
carodriguezp <crpzgz92@gmail.com>
2024-02-21 11:34:37 +0100

    4.5

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const mysql = require('mysql2/promise');
 
 // create and config server
 const server = express();
@@ -10,4 +11,45 @@ server.use(express.json());
 const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
+});
+
+//conexión a la bases de datos
+async function getConnection() {
+  //creary configurar la conexion
+  const connection = await mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'netflix',
+  });
+
+  connection.connect();
+  return connection;
+}
+
+
+//endpoint para todas las peliculas
+server.get('/movies', async (req, res) => {
+  //require para cuando envien datos
+  //response para enviar desde el server datos al front
+
+  //Obtener los datos de la bases de datos
+  // 1. Obtener la conexion
+  const conn = await getConnection();
+
+  //. 2. Consulta que quiero a la bd: obtener todas las movies
+  const queryMovies = 'SELECT * FROM movies';
+
+  //3. Ejecutar la consulta
+  const [results, fields] = await conn.query(queryMovies);
+
+  console.log(fields);
+  console.log(results);
+
+  //4. Cerra la conexión
+  conn.end();
+  res.json({
+    success: true,
+    movies: results
+  });
 });
--- web/src/services/api-movies.jsx
+++ web/src/services/api-movies.jsx
@@ -3,30 +3,37 @@
 const getMoviesFromApi = () => {
   console.log('Se están pidiendo las películas de la app');
   // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
-  return fetch('//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/empty.json')
+  return fetch('http://localhost:4000/movies', { method: "GET" })
     .then(response => response.json())
-    .then(() => {
-      // CAMBIA EL CONTENIDO DE ESTE THEN PARA GESTIONAR LA RESPUESTA DEL SERVIDOR Y RETORNAR AL COMPONENTE APP LO QUE NECESITA
-      return {
-        success: true,
-        movies: [
-          {
-            id: '1',
-            title: 'Gambita de dama',
-            genre: 'Drama',
-            image:
-              '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/gambito-de-dama.jpg'
-          },
-          {
-            id: '2',
-            title: 'Friends',
-            genre: 'Comedia',
-            image:
-              '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/friends.jpg'
-          }
-        ]
-      };
+
+    .then(data => {
+      console.log(data)
+      return data;
     });
+  // .then(() => {
+  //   // CAMBIA EL CONTENIDO DE ESTE THEN PARA GESTIONAR LA RESPUESTA DEL SERVIDOR Y RETORNAR AL COMPONENTE APP LO QUE NECESITA
+
+
+  //   return {
+  //     success: true,
+  //     movies: [
+  //       {
+  //         id: '1',
+  //         title: 'Gambito de dama',
+  //         genre: 'Drama',
+  //         image:
+  //           '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/gambito-de-dama.jpg'
+  //       },
+  //       {
+  //         id: '2',
+  //         title: 'Friends',
+  //         genre: 'Comedia',
+  //         image:
+  //           '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/friends.jpg'
+  //       }
+  //     ]
+  //   };
+  // });
 };
 
 const objToExport = {
